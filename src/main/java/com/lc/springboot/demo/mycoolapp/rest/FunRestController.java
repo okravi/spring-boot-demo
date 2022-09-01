3c8b6a7d47c32ed7b6201c369a4a49a65fbd2741
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-01 19:17:33 +0300

    progress update

--- src/main/java/com/lc/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/lc/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,19 @@ package com.lc.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	//inject properties
+	@Value("${team.name}")
+	private String teamName;
+	
+	@Value("${coach.name}")
+	private String coachName;
 	
 	@GetMapping("/")
 	public String sayHello() {
@@ -15,11 +23,16 @@ public class FunRestController {
 	
 	@GetMapping("/workout")
 	public String getDailyWorkout() {
-		return "Train a lot";
+		return "Train a lot, says " + coachName;
 	}
 	
 	@GetMapping("/fortune")
 	public String getDailyFortune() {
 		return "Today is your lucky day";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", team: " + teamName;
 	}
 }
